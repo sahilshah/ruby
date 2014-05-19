7d15bd8aa48c7cc1e42c614adb0f2ae8fde0ad9c
Sahil Shah <sshah@tonboimaging.com>
2014-05-19 19:54:35 +0530

    forgit to commit i guess, but finishes koans long bac

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -2,9 +2,21 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  
+  def roll(counts)
+    @rolls = []
+    counts.times{
+      @rolls << 1 + rand(6)
+    }
+    @rolls
+  end
+
+  def values
+    @rolls
+  end
+
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
--- koans/about_proxy_object_project.rb
+++ koans/about_proxy_object_project.rb
@@ -15,8 +15,31 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 class Proxy
   def initialize(target_object)
     @object = target_object
-    # ADD MORE CODE HERE
+    @messages = []
+    @object
   end
+
+  def method_missing(method_name, *args, &block)
+    if @object.respond_to?(method_name)
+      @messages << method_name
+      @object.send(method_name,*args, &block)
+    else
+      super(method_name, *args, &block)
+    end
+  end
+
+  def messages
+    @messages
+  end
+
+  def called?(method_name)
+    messages.include?(method_name)
+  end
+
+  def number_of_times_called(method_name)
+    messages.count(method_name)
+  end
+
 
   # WRITE CODE HERE
 end
